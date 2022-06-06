688df2c63870e6a655f3ffe1f8319420ba70afa7
Kwame Opare Asiedu <kwasiedu.5@gmail.com>
2022-06-06 13:53:23 +0000

    Upgraded AutoUssd to 4.0.0-alpha-02 and refactored usage code to match

--- app/src/main/java/com/autoussd/sample/kotlin/PaymentActivity.kt
+++ app/src/main/java/com/autoussd/sample/kotlin/PaymentActivity.kt
@@ -7,7 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.autoussd.AutoUssd
-import com.autoussd.models.DeviceSimNetworksCallback
+import com.autoussd.models.DeviceSimNetworksListener
 import com.autoussd.models.Network
 import com.autoussd.models.Result
 
@@ -21,20 +21,26 @@ class PaymentActivity : ComponentActivity() {
         setContentView(R.layout.activity_payment)
         AutoUssd.init(this)
 
-        AutoUssd.onSessionResult("callback-key") {
+        AutoUssd.getInstance().registerSessionResultListener("callback-key") {
             when (it.status) {
                 Result.Status.COMPLETED -> Log.d(TAG, "Completed")
                 Result.Status.PARSED -> Log.d(TAG, "Parsed")
                 Result.Status.INVALID_SESSION -> Log.d(TAG, "Invalid session Id")
                 Result.Status.UNSUPPORTED_SIM -> Log.d(TAG, "Unsupported SIM")
                 Result.Status.SESSION_TIMEOUT -> Log.d(TAG, "Session timed-out")
-                Result.Status.MENU_CONTENT_MISMATCH -> Log.d(TAG, "USSD content did not match menu content")
-                Result.Status.ACCOUNT_SUBSCRIPTION_EXPIRED -> Log.d(TAG, "Account subscription expired")
+                Result.Status.MENU_CONTENT_MISMATCH -> Log.d(
+                    TAG,
+                    "USSD content did not match menu content"
+                )
+                Result.Status.ACCOUNT_SUBSCRIPTION_EXPIRED -> Log.d(
+                    TAG,
+                    "Account subscription expired"
+                )
                 Result.Status.UNKNOWN_ERROR -> Log.d(TAG, "Unknown error occurred")
             }
         }
 
-        AutoUssd.getDeviceSimNetworks(object: DeviceSimNetworksCallback{
+        AutoUssd.getInstance().getDeviceSimNetworks(object : DeviceSimNetworksListener {
             override fun onResult(networks: List<Network>) {
                 networks.forEach {
                     Log.d(TAG, it.toString())
@@ -49,8 +55,8 @@ class PaymentActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        AutoUssd.offSessionResult("callback-key")
-        AutoUssd.dispose()
+        AutoUssd.getInstance().unregisterSessionResultListener("callback-key")
+        AutoUssd.getInstance().dispose()
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -70,12 +76,12 @@ class PaymentActivity : ComponentActivity() {
             return Toast.makeText(this, "All fields are required", Toast.LENGTH_LONG).show()
         }
 
-        AutoUssd.executeSession(
-            "60a53f240000000000000000",
-            arrayOf(
-                recipientNumber,
-                amount,
-                reference,
+        AutoUssd.getInstance().executeSession(
+            "629e05e1751c102d57c53c0d",
+            mapOf(
+                "number" to recipientNumber,
+                "amount" to amount,
+                "reference" to reference,
             ),
             null
         )
